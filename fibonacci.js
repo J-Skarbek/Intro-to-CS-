1ead90bbe8cbb2fc5a152fef5754e7cc3733bd5c
J-Skarbek <john@snarkmediaagency.com>
2023-01-22 16:48:28 -0600

    Create recursive Fibonacci funcion

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,30 @@
 function fibonaccciIterative(n) {
   const values = [0, 1];
-  const sumTwoPriorNums = () => Number(values.slice(-1)) + Number(values.slice(-2,-1));
+  const sumTwoPriorNums = () => Number(values.slice(-1)) + Number(values.slice(-2, -1));
 
   while (n > 2) {
     values.push(sumTwoPriorNums());
     n -= 1;
   }
-  
+
   return values;
+};
+
+function fibonaccciRecursive(n) {
+  const values = [0, 1];
+  const sumTwoPriorNums = () => Number(values.slice(-1)) + Number(values.slice(-2, -1));
+
+  const recursion = () => {
+    if ( n < 2) {
+      return values;
+    } else {
+      values.push(sumTwoPriorNums());
+      console.log(n);
+      n -= 1;
+      recursion(n);
+      return values;
+    }; 
+  };
+  return recursion();
 };
 
