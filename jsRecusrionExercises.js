ec077d339348904ed70cdcdc1d67155d2199380f
J-Skarbek <john@snarkmediaagency.com>
2023-01-25 17:37:00 -0600

    start with bare-bone facotry functions and meaningless array for linked list exercise

--- jsRecusrionExercises.js
+++ jsRecusrionExercises.js
@@ -1,16 +1,52 @@
+const testArray = [
+  41, 
+  42, 
+  89, 
+  71, 
+  7, 
+  92, 
+  24, 
+  88, 
+  65, 
+  88, 
+  100, 
+  57,
+  30, 
+  2, 
+  10, 
+  70, 
+  63, 
+  26, 
+  57, 
+  94,
+  40, 
+  36, 
+  61, 
+  1, 
+  58
+];
+
+const sampleObject = {
+  data: {
+    info: {
+      stuff: {
+        thing: {
+          moreStuff: {
+            number: 44,
+            something: 'Sample string'
+          }
+        }
+      }
+    }
+  }
+};
+
 function sumRange(x) {
   if (x == 1) {
     return 1;
   }
   return x + (sumRange(x -1))
 }
-
-// function power(base, exponent) {
-//   if (exponent === 0) {
-//     return 1;
-//   }
-//   return base ** exponent;
-// }
 
 function power(base, exponent) {
   if (exponent === 0) {
@@ -22,4 +58,18 @@ function power(base, exponent) {
 function factorial(x) {
   if (x <= 1) return 1;
   return x * factorial(x - 1);
+}
+
+function productOfArray(array) {
+  return (array.length === 0) ? 0 : array[0] + productOfArray(array.slice(1));
+}
+
+function contains(object, searchValue) {
+  for (const property in object) {
+    if (typeof object[property] === 'object') {
+      return contains(object[property], searchValue);
+    };
+    if (object[property] === searchValue) return true;
+  }
+  return false;
 }
---
+++ linkedList.js
@@ -0,0 +1,39 @@
+const fourtyFive = [
+ 'lil wayne',
+ 'drake',
+ 'nikki minage',
+ 'childish gambino',
+ 'two chainz',
+ 'mac dre',
+ 'emeniem',
+ 'jay-z',
+ 'rick ross',
+ 'future',
+ 'travis scott',
+ 'timbaland',
+ 'migos',
+ 'rich boy',
+ 'soulja boy'
+];
+
+const LinkedList = () => {
+  const sampleMethod = () => console.log('testing');
+
+  const append = value => {};
+
+  const prepend = value => {};
+
+  const size = () => {
+    let nodes = 0;
+    
+  }
+
+  return {
+    sampleMethod
+  };
+};
+
+const Node = () => {
+  const value = () => null;
+  const nextNode = null;
+}
