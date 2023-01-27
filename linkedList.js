a299374d9f2403621be62acdc7988d74cfdad1ad
J-Skarbek <john@snarkmediaagency.com>
2023-01-27 14:23:38 -0600

    Complete troubleshoot of list.append function

--- linkedList.js
+++ linkedList.js
@@ -1,23 +1,8 @@
 const LinkedList = title => {
-
   let head = null;
   let length = 0;
 
   const sampleMethod = () => console.log('testing');
-
-  // const append = value => {
-  //   const newNode = Node(value);
-  //   length++;
-  //   console.log(newNode.value());
-  //   console.log(length)
-  //   return newNode;
-  // };
-
-  // const append = newNode => {
-  //   if (head === null) prepend(newNode);
-  //   newNode = Node(newNode);
-
-  // };
 
   //Adds an new node to the beginnning of the linked list
   function prepend(newNode) {
@@ -33,35 +18,25 @@ const LinkedList = title => {
   //Adds a node to the end of the linked list
   function append(newNode) {
     if (this.head == null) {
-      prepend(newNode);
-    } else {
-      let node = Node(newNode, null);
-      let n = this.head;
-      while (n.testFunctionTwo !== null) {
-        console.log(n.testfunction);
-        n = n.testFunctionTwo;
-        console.log(n + ' second test');
-      };
-      n.testfunctionTwo = node;
-      console.log(n.testfunctionTwo, n.testfunction)
-    };
+      return prepend(newNode);
+    }
+    let node = Node(newNode, null);
+    let tail = this.head;
+    while (tail.testFunctionTwo != null) {
+      tail = tail.testFunctionTwo;
+      console.log(tail);
+    };    
+    tail.testFunctionTwo = node;
   }
 
   //Traverses the linked list and console log's the found values of each item 
   function find() {
     let n = this.head;
-    console.log(typeof n, n.value(), n.next());
     while (n != null) {
       console.log(`Linked List Item: ${n.testfunction}`);
       n = n.testFunctionTwo;
     };
   }
-
-  // const tail = () => {
-  //   while (head !== null) {
-  //     if 
-  //   }
-  // }
 
   function getTheHead(){
     return this.head;
@@ -77,7 +52,6 @@ const LinkedList = title => {
     size,
     find,
     length,
-    // tail,
   }
 }
 
