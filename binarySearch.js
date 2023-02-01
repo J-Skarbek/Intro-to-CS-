b4e9f66d4892cbd181d21cb5318638c89fdaa39c
J-Skarbek <john@snarkmediaagency.com>
2023-01-31 18:04:30 -0600

    Start on node factory function, create sample array

---
+++ binarySearch.js
@@ -0,0 +1,35 @@
+const bsArray = [
+  1001,
+  754,
+  222,
+  0,
+  587,
+  11,
+  4,
+  5,
+  88,
+  600,
+  999,
+  555,
+  420,
+  69,
+  111,
+  8,
+  99,
+  876,
+  700
+];
+
+const Node = (name, data) => {
+  this.left = null;
+  this.right = null;
+  this.testing = () => console.log('hello bob.')
+
+  return {
+    name,
+    data,
+    left,
+    right,
+    testing
+  }
+}
