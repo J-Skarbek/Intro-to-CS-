d9bca168726657cf617b7b4e0aeb7b5e06ec3683
J-Skarbek <john@snarkmediaagency.com>
2023-02-02 15:57:47 -0600

    Seperate concerns between returning the sorted array and getting the root node from the sorted array

--- binarySearch.js
+++ binarySearch.js
@@ -31,7 +31,7 @@ const Node = (name, data) => {
     left,
     right,
     testing
-  }
+  };
 }
 
 const BinarySearchTree = (arrayName) => {
@@ -40,6 +40,7 @@ const BinarySearchTree = (arrayName) => {
 
   let root = null;
 
+  //helper function used by the mergeSort() method
   function _midPoint(array) {
     if (array.length % 2 === 0) {
       const mid = array.length / 2;
@@ -50,6 +51,7 @@ const BinarySearchTree = (arrayName) => {
     };
   }
 
+  //helper function used by the mergeSort() method
   function _merge(lArray, rArray) {
     let resultArray = [];
     let leftIndex = 0;
@@ -62,7 +64,7 @@ const BinarySearchTree = (arrayName) => {
       } else {
         resultArray.push(rArray[rightIndex]);
         rightIndex++;
-      }
+      };
     };
     return resultArray
             .concat(lArray.slice(leftIndex))
@@ -73,28 +75,54 @@ const BinarySearchTree = (arrayName) => {
     console.log(array.length);
     console.table(array);
   }
-  
+
+  //Performs a mergesort on the input array and is called by the createRoot() method
   function mergeSort(array) {
     if (array.length <= 1) return array;
     const leftArray = array.slice(0, _midPoint(array));
     const rightArray = array.slice(_midPoint(array));
-    console.log(`left array: ${leftArray} --- right array: ${rightArray}`);
+    // console.log(`left array: ${leftArray} --- right array: ${rightArray}`);
 
     return _merge(
       mergeSort(leftArray), mergeSort(rightArray)
     );
   }
 
-  function _createRoot() {
-    const sortedArray = this.mergeSort(this.array);
-    const firstRoot = Node(sortedArray[this._midPoint(sortedArray)]);
-    console.table(sortedArray);
-    return firstRoot;
+  function _mergeSortArray() {
+    return this.mergeSort(this.array);
   }
 
+  //runs a mergesort and retruns the base root node from the the now-sorted array
+  function _createRoot() {
+    const newArray = this._mergeSortArray();
+    const root = Node(newArray[this._midPoint(newArray)]);
+    return root;
+  };
 
-  function buildTree() {
+  function buildTree(array, start, end) {
+    if (start > end) return null;
+    // let sortArr = mergeSort(array);
+    let mid = _midPoint(_createRoot().sortedArray);
+    let node = this._createRoot;
+    // let node = Node(mid);
+    node.left = buildTree(array, start, mid - 1);
+    node.right = buildTree(array, mid + 1, end)
+    return node;
+  }
 
+  function runBuildTree() {
+    let arrLength = this.array.length;
+    this.root = this.buildTree(this.array, 0, arrLength -1);
+    displayNodes(this.root);
+  }
+
+  function displayNodes(node) {
+    if (node === null) return;
+    console.log(node);
+    displayNodes(node.left);
+    console.log(node.left);
+    displayNodes(node.right);
+    console.log(node.left);
   }
 
   function displayArray() {
@@ -111,5 +139,8 @@ const BinarySearchTree = (arrayName) => {
     array,
     root,
     buildTree,
+    runBuildTree,
+    displayNodes,
+    _mergeSortArray,
   }
 }
